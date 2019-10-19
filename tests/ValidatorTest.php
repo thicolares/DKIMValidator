ff8bebc65116fb6d7049fc06b739f0c6b55706a6
Thiago Colares <thicolares@gmail.com>
2019-10-19 12:22:29 -0300

    #2 Add minimal test suite configuration
    - And example test

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPMailer\DKIMValidator\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertEquals(1,1);
+    }
+}
